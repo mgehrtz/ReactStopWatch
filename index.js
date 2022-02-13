62d0def3142ee09109c8539194ea9bf18370d6ee
Michael Gehrtz <m.gehrtz@gmail.com>
2022-02-12 20:49:22 -0600

    Create stopwatch component.

---
+++ index.js
@@ -0,0 +1,68 @@
+class Stopwatch extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            msPassed: 0
+        }
+
+        this.timer = null;
+
+        this.start = this.start.bind(this);
+        this.stop = this.stop.bind(this);
+        this.reset = this.reset.bind(this);
+    }
+
+    start() {
+        if (!this.timer) {
+            let startTime = Date.now();
+            this.timer = setInterval(() => {
+                const stopTime = Date.now();
+                const msPassed = stopTime - startTime + this.state.msPassed;
+
+                this.setState({
+                    msPassed,
+                });
+
+                startTime = stopTime;
+            }, 250);
+        }
+    }
+
+    stop() {
+        window.clearInterval(this.timer);
+        this.timer = null;
+    }
+
+    reset() {
+        this.stop();
+        this.setState({
+            msPassed: 0
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h2 className="border px-3 py-4 rounded my-3 mx-auto text-center" style={{maxWidth: "300px"}}>
+                    {Math.floor(this.state.msPassed / 1000)} s
+                </h2>
+                <div className="d-flex justify-content-center">
+                    <button className="btn btn-outline-primary mr-2">
+                        start
+                    </button>
+                    <button className="btn btn-outline-danger mr-2">
+                        stop
+                    </button>
+                    <button className="btn btn-outline-warning">
+                        reset
+                    </button>
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Stopwatch />,
+    document.getElementById('root')
+);
